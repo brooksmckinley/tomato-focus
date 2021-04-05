fc34d00a1c7d1b9caab4dce4ac0a26d7ab4c4945
Brooks McKinley <brooksmckinley@yahoo.com>
2021-04-05 18:08:54 -0400

    Timer now switches between work, short break, and long break

--- src/Clock.js
+++ src/Clock.js
@@ -1,7 +1,14 @@
 // TODO: Set this to a reasonable value, and add configurable pomodoro times
 const WORK_TIME = 5
 const SHORT_BREAK = 3
+const POMODOROS_BREFORE_LONG = 4
 const LONG_BREAK = 5
+
+const CurrentTimer = {
+	WORK: 0,
+	SHORT_BREAK: 1,
+	LONG_BREAK: 2,
+}
 
 function pad(num, padding) {
 	let result = ""
@@ -19,22 +26,64 @@ function secondsToClockTime(seconds) {
 }
 
 class Clock extends React.Component {
+
 	constructor(props) {
 		super(props)
 		this.onStart = this.onStart.bind(this)
 		this.onStop = this.onStop.bind(this)
 		this.state = {
-			timer: 0
+			timer: 0,
+			pomodoros: 0,
+			currentTimer: CurrentTimer.WORK,
 		}
 	}
+
 	onFinish() {
-		this.props.onFinish()
+		// Only activate callback if work period was just completed
+		if (this.state.currentTimer == CurrentTimer.WORK)
+			this.props.onFinish()
+
 		this.onStop() // stops the timer from counting below zero
+
+		if (this.state.currentTimer === CurrentTimer.WORK) {
+			let newPomodoros = this.state.pomodoros + 1
+			if (newPomodoros % POMODOROS_BREFORE_LONG === 0) {
+				this.setState({
+					pomodoros: newPomodoros,
+					currentTimer: CurrentTimer.LONG_BREAK
+				})
+			}
+			else {
+				this.setState({
+					pomodoros: newPomodoros,
+					currentTimer: CurrentTimer.SHORT_BREAK
+				})
+			}
+		}
+		else {
+			this.setState({
+				currentTimer: CurrentTimer.WORK
+			})
+		}
 	}
+
 	onStart() {
+		let newTimer = -1
+		switch (this.state.currentTimer) {
+			case CurrentTimer.WORK:
+				newTimer = WORK_TIME
+				break
+			case CurrentTimer.SHORT_BREAK:
+				newTimer = SHORT_BREAK
+				break
+			case CurrentTimer.LONG_BREAK:
+				newTimer = LONG_BREAK
+				break
+		}
 		this.setState({
-			timer: WORK_TIME
+			timer: newTimer
 		})
+		
 		// Creates the interval to tick the timer once a second if it's not already there
 		if (!this.timerInterval) {
 			this.timerInterval = setInterval(() => {
@@ -48,6 +97,7 @@ class Clock extends React.Component {
 			}, 1000)
 		}
 	}
+
 	onStop() {
 		this.setState({
 			timer: 0
@@ -59,13 +109,16 @@ class Clock extends React.Component {
 			this.timerInterval = undefined
 		}
 	}
+
 	render() {
 		return <div>
 			<h1>{secondsToClockTime(this.state.timer)}</h1>
+			<p>Current state: {this.state.currentTimer}</p>
 			<button onClick={this.onStart}>Start</button>
 			<button onClick={this.onStop}>Stop</button>
 		</div>
 	}
+
 }
 
 export default Clock

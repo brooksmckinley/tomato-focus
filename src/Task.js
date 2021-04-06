88b4e9771a6294e99511434340c8554154db5c25
Brooks McKinley <brooksmckinley@yahoo.com>
2021-04-06 12:11:56 -0400

    Checking tasks now updates state properly

--- src/Task.js
+++ src/Task.js
@@ -1,13 +1,27 @@
 class Task extends React.Component {
+
 	constructor(props) {
 		super(props)
+		this.state = {
+			completed: props.completed
+		}
+
+		this.onChange = this.onChange.bind(this)
 	}
+
+	onChange(event) {
+		this.setState({
+			completed: event.target.checked
+		})
+	}
+
 	render() {
 		return <div>
-			<input type="checkbox" checked={this.props.completed}></input>
+			<input type="checkbox" onChange={this.onChange} checked={this.state.completed}></input>
 			<label>{this.props.name}</label>
 		</div>
 	}
+
 }
 
 export default Task

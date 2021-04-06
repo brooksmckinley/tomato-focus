9c6d7f93b943012f5c01b36f3e919fc5c37a5086
Brooks McKinley <brooksmckinley@yahoo.com>
2021-04-06 12:20:07 -0400

    Added multiple views to App
    Going to be used for implementing the text editor, game, and settings menus

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import Clock from "./Clock.js"
 import TaskList from "./TaskList.js"
 
+const View = {
+	TIMER: 0,
+	SETTINGS: 1,
+}
+
 class App extends React.Component {
+
 	constructor(props) {
 		super(props)
 		this.state = {
 			score: 0,
+			view: View.TIMER,
 		}
 		this.onCompletedPomodoro = this.onCompletedPomodoro.bind(this)
 	}
+
+	setView(newView) {
+		this.setState({
+			view: newView,
+		})
+	}
+
+	getView() {
+		if (this.state.view == View.TIMER) {
+			return <div>
+				<p>Current score: {this.state.score}</p>
+				<Clock onFinish={this.onCompletedPomodoro}/>
+				<TaskList />
+			</div>
+		}
+		else if (this.state.view == View.SETTINGS) {
+			// TODO: Add settings menu
+			return <div></div>
+		}
+	}
+
 	onCompletedPomodoro() {
 		this.setState({
 			score: this.state.score + 1
 		})
 	}
+
 	render() {
+		let currentView = this.getView()
 		return <div>
-			<p>Current score: {this.state.score}</p>
-			<Clock onFinish={this.onCompletedPomodoro}/>
-			<TaskList />
+			<button onClick={() => this.setView(View.TIMER)}>Timer</button>
+			<button onClick={() => this.setView(View.SETTINGS)}>Settings</button>
+			{currentView}
 		</div>
 	}
+
 }
 
 ReactDOM.render(<App />, document.getElementById("app"))

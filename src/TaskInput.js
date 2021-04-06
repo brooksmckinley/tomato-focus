f466eff5bad55a8cb4d6e502bc04c1a2c43f0bb2
Brooks McKinley <brooksmckinley@yahoo.com>
2021-04-06 12:05:23 -0400

    TaskInput now can be used to add tasks to the list

--- src/TaskInput.js
+++ src/TaskInput.js
@@ -1,10 +1,29 @@
 class TaskInput extends React.Component {
 	constructor(props) {
 		super(props)
+		this.state = {
+			value: "",
+		}
+
+		this.submit = this.submit.bind(this)
+		this.updateValue = this.updateValue.bind(this)
+	}
+
+	submit(event) {
+		event.preventDefault()
+		this.props.onSubmit(this.state.value)
+	}
+
+	updateValue(event) {
+		this.setState({
+			value: event.target.value,
+		})
 	}
 
 	render() {
-		return <input></input>
+		return <form onSubmit={this.submit}>
+			<input onChange={this.updateValue} type="text"></input>
+		</form>
 	}
 }
 
--- src/TaskList.js
+++ src/TaskList.js
@@ -9,27 +9,26 @@ class TaskList extends React.Component {
 			lastTaskID: 0,
 			tasks: [],
 		}
-		this.state.tasks = this.state.tasks.concat([new TaskItem(this.lastTaskID, "Test task", false)])
-		console.log(this.state.tasks[0])
+
+		this.addTask = this.addTask.bind(this)
 	}
 
-	addTask(task) {
+	addTask(name) {
 		this.setState((state, props) => {
 			return {
 				lastTaskID: state.lastTaskID + 1,
-				tasks: state.tasks.concat(task),
+				tasks: state.tasks.concat(new TaskItem(state.lastTaskID, name, false)),
 			}
 		})
 	}
 
 	render() {
 		let taskList = this.state.tasks.map((task) => {
-			console.log(task.taskContent)
 			return <Task key={""+task.id} name={task.taskContent} completed={task.isCompleted} />
 		})
 		return <div>
 			{taskList}
-			<TaskInput />
+			<TaskInput onSubmit={this.addTask} />
 		</div>
 	}
 
